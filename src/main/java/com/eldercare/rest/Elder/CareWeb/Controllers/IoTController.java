f063bf07e75061365ad0ffa43781bfe8f972b4e8
mashkarharis <mashkarharis@gmail.com>
2022-05-15 19:09:50 +0530

    Mobile Serive Almost Done

--- src/main/java/com/eldercare/rest/Elder/CareWeb/Controllers/IoTController.java
+++ src/main/java/com/eldercare/rest/Elder/CareWeb/Controllers/IoTController.java
@@ -1,10 +1,13 @@
 package com.eldercare.rest.Elder.CareWeb.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +56,17 @@ public class IoTController {
 		
 		
 		// TODO: ADD NOTIFICATION SENDING
+		
+	}
+	
+	@GetMapping("/iot/protected/getmacs")
+	public List<String> getMACs() throws Exception {
+		List<String> macs=new ArrayList<>();
+		List<Coordinate> coors=ioTService.getCoordinates();
+		for (Coordinate coordinate : coors) {
+			macs.add(coordinate.getMac());
+		}
+		return macs;
 		
 	}
 }
